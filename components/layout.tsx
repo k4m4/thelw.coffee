01b352f2bb931954c64c2bd5bac074a2a1a2af72
k4m4 <nikolaskam@gmail.com>
2021-07-14 22:00:21 +0300

    Fix indentation in Layout component

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,17 +4,16 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Layout({ pageTitle, children }: { pageTitle?: string, children: ReactNode }) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>{pageTitle && `${pageTitle} | `}Thelw Coffee</title>
-        <meta name="description" content="Greek Coffee acronym parser" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-	  	{children}
-	  </main>
-    </div>
-  )
+	return (
+		<>
+			<Head>
+				<title>{pageTitle && `${pageTitle} | `}Thelw Coffee</title>
+				<meta name="description" content="Greek Coffee acronym parser" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main className={styles.main}>
+				{children}
+			</main>
+		</>
+	)
 }
