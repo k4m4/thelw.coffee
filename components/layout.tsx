eb78c2dc28866d04782eb6ec983aebd78037a70a
k4m4 <nikolaskam@gmail.com>
2021-07-16 12:21:43 +0300

    Update Layout component

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,48 @@
 import { ReactNode } from 'react'
-import Head from 'next/head'
+import NextHead from 'next/head'
+import { useRouter } from 'next/router'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+
+const SITE_NAME = 'Thelw Coffee'
+const SITE_DOMAIN = 'thelw.coffee'
+const DESCRIPTION = 'Greek Coffee acronym parser'
+const LOCALE = 'el_GR'
+
+type HeadProps = {
+	pageTitle?: string
+	ogImage?: string
+}
+
+const Head = ({ pageTitle, ogImage }: HeadProps) => {
+	const router = useRouter()
+	const title: string = `${pageTitle && `${pageTitle} | `}${SITE_NAME}`
+	const url = router.asPath
+	return (
+		<NextHead>
+			<title>{title}</title>
+			<meta name="description" content={DESCRIPTION} />
+			<meta property="og:title" content={title} />
+			<meta property="og:description" content={DESCRIPTION} />
+			{ogImage && (
+				<>
+					<meta property="og:image" content={ogImage} />
+					<meta name="twitter:image" content={ogImage} />
+				</>
+			)}
+			{/* <link rel="icon" href="/favicon.ico" /> */}
+			<meta property="og:locale" content={LOCALE} />
+			<meta property="og:site_name" content={SITE_NAME} />
+			<meta property="og:type" content="website" />
+			<meta property="og:url" content={url} />
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta property="twitter:domain" content={SITE_DOMAIN} />
+			<meta property="twitter:url" content={url} />
+			<meta name="twitter:title" content={title} />
+			<meta name="twitter:description" content={DESCRIPTION} />
+		</NextHead>
+	)
+}
 
 type LayoutProps = {
 	pageTitle?: string
@@ -9,17 +50,10 @@ type LayoutProps = {
 	children: ReactNode
 }
 
-export default function Layout({ pageTitle, ogImage, children }: LayoutProps) {
+export default function Layout({ children, ...headProps }: LayoutProps) {
 	return (
 		<>
-			<Head>
-				<title>{pageTitle && `${pageTitle} | `}Thelw Coffee</title>
-				<meta name="description" content="Greek Coffee acronym parser" />
-				{ogImage && (
-					<meta property="og:image" content={ogImage} />
-				)}
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+			<Head {...headProps} />
 			<main className={styles.main}>
 				{children}
 			</main>
