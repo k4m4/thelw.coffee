e955b5176cf26f9489a0d5e586a6d9c8d02a9f7a
k4m4 <nikolaskam@gmail.com>
2021-07-18 16:50:14 +0300

    Use server-side rendering

--- pages/[acronym].tsx
+++ pages/[acronym].tsx
@@ -1,13 +1,15 @@
-import { useRouter } from 'next/router'
+import { GetServerSideProps } from 'next'
 import Layout from '../components/layout'
 import parseCoffeeAcronym, { Acronym as CoffeeAcronym } from '../lib'
 import styles from '../styles/Home.module.css'
 
 const SITE_BASE_URL = 'https://thelw.coffee'
 
-const Acronym = () => {
-	const router = useRouter()
-	const { acronym } = router.query
+type AcronymProps = {
+	acronym: string
+}
+
+const Acronym = ({ acronym }: AcronymProps) => {
 	if (!acronym || typeof acronym !== 'string') {
 		return null
 	}
@@ -31,6 +33,20 @@ const Acronym = () => {
 		)
 	} catch (error) {
 		return <Layout>{error.message}</Layout>
+	}
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+	if (!params?.acronym) {
+		return {
+			notFound: true,
+		}
+	}
+
+	return {
+		props: {
+			acronym: params.acronym,
+		},
 	}
 }
 
